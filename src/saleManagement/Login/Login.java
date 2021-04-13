214ac2d83587f6782752c1f039fc06137d020400
Thaehan <19021236@vnu.edu.vn>
2021-04-14 00:35:49 +0700

    Initial commit

---
+++ src/saleManagement/Login/Login.java
@@ -0,0 +1,30 @@
+package saleManagement.Login;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+public class Login extends Application{
+
+    public void start(Stage primaryStage) throws Exception {
+        try{
+            Parent parentRoot = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(parentRoot);
+            parentRoot.getStylesheets().add(getClass().getResource("stylesheet.css").toString());
+            primaryStage.setTitle("Login SalesManger");
+            primaryStage.setIconified(false);
+            primaryStage.setResizable(false);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/saleManagement/Login/LoginController.java
@@ -0,0 +1,25 @@
+package saleManagement.Login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LoginController {
+    @FXML
+    private TextField account;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button recoveryButton;
+    @FXML
+    private ImageView bgimage;
+
+    public void buttonPressed(ActionEvent event) {
+        System.out.println("button clicked");
+    }
+}
